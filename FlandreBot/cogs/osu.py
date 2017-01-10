877c9bff69b52e5fb810df3dd149f5647ff7a9c3
Ma-wa-re <jakey8180@gmail.com>
2017-01-10 21:10:38 +0000

    Tidied up current osu code

--- FlandreBot/cogs/osu.py
+++ FlandreBot/cogs/osu.py
@@ -12,43 +12,37 @@ class osu():
     '''
     def __init__(self, bot):
         self.bot = bot
-        self.modes = {0: 'osu!', 1: 'Taiko', 2: 'CtB', 3: 'osu!mania'}
-
-    def mapStatus(self, status):
-        ''' Return is a map is ranked and that '''
-        if status == '0':
-            return 'Pending'
-        elif status == '-1':
-            return 'WIP'
-        elif status == '-2':
-            return 'Graveyard'
-        elif status == '1':
-            return 'Ranked'
-        elif status == '2':
-            return 'Approved'
-        elif status == '3':
-            return 'Qualified'
-        elif status == '4':
-            return 'Loved'
+        # Gamemodes based off mode mumber from api
+        self.modes = {'0': 'osu!', '1': 'Taiko', '2': 'CtB', '3': 'osu!mania'}
+        # Map Ranking status based off number from api
+        self.map_status = {'-2': 'Graveyard', '-1': 'WIP','0': 'Pending', '1': 'Ranked', '2': 'Approved', '3': 'Qualified', '4': 'Loved'}
 
     async def getBeatmap(self, message):
+        ''' Get Beatmap Info '''
         if 'osu' not in message.channel.name.lower():
             return
-        ''' Get Beatmap Info '''
+
         words = message.content.split(' ')
         mods = ''
-        percent = 'Fill'
-        mode = 0
+        percent = ''
+        url = None
+
         for word in words:
             if 'osu.ppy.sh/b/' in word:
-                osu_link = word
+                # Get map and make request url
+                mapID = word.split('/b/')[1].split('&')[0] 
+                url = 'https://osu.ppy.sh/api/get_beatmaps?k={0}&b=' + str(mapID)
                 beatmapSet = False
-                mode = osu_link.split('m=')[1]
+            
             elif 'osu.ppy.sh/s/' in word:
-                osu_link = word
+                # Get mapset and make request url
+                mapID = word.split('/s/')[1]
+                url = 'https://osu.ppy.sh/api/get_beatmaps?k={0}&s=' + str(mapID)
                 beatmapSet = True 
+            
             elif '+' in word:
                 mods = word.upper()
+            
             elif '%' in word:
                 percent = word   
         
@@ -89,49 +83,62 @@ class osu():
         if mods != '':
             mods = '+' + mods
         
-        # Get request url
-        if beatmapSet:
-            # Beatmap Set
-            mapID = osu_link.split('/s/')[1]
-            url = 'https://osu.ppy.sh/api/get_beatmaps?k={0}&s=' + str(mapID)
-        else:
-            # Single Map
-            mapID = osu_link.split('/b/')[1].split('&')[0] 
-            url = 'https://osu.ppy.sh/api/get_beatmaps?k={0}&b=' + str(mapID)
-
-        # Get beatmap Info
+        # Get Api Info
         with aiohttp.ClientSession() as aioclient:
             async with aioclient.get(url.format(self.bot.config['osukey'])) as resp:
                 data = await resp.json()
+                status_code = resp.status
 
-        if data:
+        # Check if there is data and the request was sucessful
+        if data and status_code == 200:
+            # Set discord embed colour, beatmap(s) state, split length into mins and seconds
             colour = Colour(16738740)
-            rank = self.mapStatus(data[0]['approved'])       
+            state = self.map_status[data[0]['approved']]  
             m, s = divmod(int(data[0]['total_length']), 60)
+            # Create Embed
+            osuembed = Embed(type='rich', colour=colour)
+            
             if beatmapSet:
-                desc = ('Artist: {0[0][artist]}\n'
-                        'Creator: {0[0][creator]}\n'
-                        'BPM: {0[0][bpm]}\n'
-                        'Source: {0[0][source]}\n'
-                        'State: {1}\n'
-                        'Length: {2}:{3}\n'
-                        'Number of difficulties: {4}'
-                        )
-                embed = Embed(type='rich', description=desc.format(data, rank, m, s, len(data)), colour=colour)
+                # Beatmap Set
+                # Add embed fields
+                osuembed.add_field(name='Artist', value=data[0].get('artist', 'Unknown'))
+                osuembed.add_field(name='Creator', value=data[0].get('creator','Unknown'))
+                osuembed.add_field(name='BPM', value=data[0].get('bpm', 'Unknown'))
+                # Check for a source
+                if data[0]['source']:
+                    osuembed.add_field(name='Source', value=data[0].get('source', 'Unknown'))
+                else:
+                    osuembed.add_field(name='Source', value='Unknown')
+                osuembed.add_field(name='State', value=state)
+                length = str(m) + ':'+ str(s) + 's'
+                osuembed.add_field(name='Length', value=length)
+                
+                # Get each difficulty name and make it into a nice string
+                diffs = []
+                for diff in data:
+                    diffs.append(diff['version'])
+
+                osuembed.add_field(name='Difficulties', value=', '.join(diffs))
+            
             else:
+                # BeatMap
+                # Get stars and max combo (set to 1500 if not found)
                 stars = float(data[0]['difficultyrating'])
-                maxcombo = 0
+                maxcombo = 1500
                 unsure = False
                 try:
                     maxcombo = int(data[0]['max_combo'])
                 except:
-                    maxcombo = 1500
                     unsure = True
+                
+                # Get cs, od , ar and hp 
                 cs = float(data[0]['diff_size'])
                 od = float(data[0]['diff_overall'])
                 ar = float(data[0]['diff_approach'])
-                hp = float(data[0]['difficultyrating'])
-                if int(data[0]['mode']) is 0:
+                hp = float(data[0]['diff_drain'])
+                
+                if data[0]['mode'] is '0':
+                    # osu! std
                     # Get beatmap for PP 
                     with aiohttp.ClientSession() as aioclient:
                         async with aioclient.get('http://osu.ppy.sh/osu/' + mapID) as resp:
@@ -148,57 +155,58 @@ class osu():
                     del beatmap
                     pp = stdout.decode().split('\n')[-2].strip()
                 
-                if int(data[0]['mode']) is 3:
-                    # Get beatmap for PP
-                    with aiohttp.ClientSession() as aioclient:
-                        async with aioclient.get('http://osu.ppy.sh/osu/' + mapID) as resp:
-                            beatmap = await resp.read()
-                       
+                elif data[0]['mode'] is '3':
+                    # osu! Mania                       
                     # Calculate pp
                     
                     ODW = 64 - (3 * od)
                     AccValue = pow((150 / ODW) * pow(int(percent[:-1])/100,16),1.8)*2.5*min(1.15,pow(maxcombo/1500,0.3))
                     StrainBase = pow(5 * max(1, stars/0.0825) - 4, 3)/ 110000 * (1 + 0.1 * min(maxcombo/1500, 1))
                     StrainMult = 1
-                    pp = pow(pow(AccValue,1.1) + pow(StrainBase * StrainMult, 1.1), (1/1.1)) * 1.1
+                    pp = str(pow(pow(AccValue,1.1) + pow(StrainBase * StrainMult, 1.1), (1/1.1)) * 1.1) + 'pp'
+
+                else:
+                    # osu! ctb or taiko
+                    pp = None 
                 
                 if mods != '':
                     mods = mods.replace('+', '')
                 else:
                     mods = 'No Mods'
-            
-                desc = ('Artist: {0[0][artist]}\n'
-                        'Creator: {0[0][creator]}\n'
-                        'Difficulty: {0[0][version]}\n'
-                        'BPM: {0[0][bpm]}\n'
-                        'Source: {0[0][source]}\n'
-                        'State: {1}\n'
-                        'Length: {2}:{3}\n'
-                        'Stars: {4:.2f}★\n'
-                        'Max Combo: {5}\n'
-                        'CS: {6:.2f}\n'
-                        'OD: {7:.2f}\n'
-                        'AR: {8:.2f}\n'
-                        'HP: {9:.2f}\n'
-                        )
-                if int(data[0]['mode']) is 0:
-                    desc += 'PP: {10} with {11} ({12})'
-                    embed = Embed(type='rich', description=desc.format(data, rank, m, s, stars, maxcombo, cs, od, ar, hp, pp, mods, percent), colour=colour)
-                elif int(data[0]['mode']) is 3:
-                    if unsure:
-                        desc += 'PP: {10} with {11} ({12})\nMax Combo is null in osu api (based on 1500 combo)'
-                        embed = Embed(type='rich', description=desc.format(data, rank, m, s, stars, maxcombo, cs, od, ar, hp, pp, mods, percent), colour=colour)
-                    else:
-                        desc += 'PP: {10} with {11} ({12})'
-                        embed = Embed(type='rich', description=desc.format(data, rank, m, s, stars, maxcombo, cs, od, ar, hp, pp, mods, percent), colour=colour)
+                           
+                osuembed.add_field(name='Artist', value=data[0].get('artist', 'Unknown'))
+                osuembed.add_field(name='Creator', value=data[0].get('creator','Unknown'))
+                osuembed.add_field(name='BPM', value=data[0].get('bpm', 'Unknown'))
+                # Check for a source
+                if data[0]['source']:
+                    osuembed.add_field(name='Source', value=data[0].get('source', 'Unknown'))
                 else:
-                    desc += 'PP: Cannot be calculated using oppai'
-                    embed = Embed(type='rich', description=desc.format(data, rank, m, s, stars, maxcombo, cs, od, ar, hp), colour=colour)
-                embed.set_footer(text='Mode: {0}'.format(self.modes[int(mode)]))
+                    osuembed.add_field(name='Source', value='Unknown')
+                osuembed.add_field(name='State', value=state)
+                length = str(m) + ':'+ str(s) + 's'
+                osuembed.add_field(name='Length', value=length)
+                osuembed.add_field(name='Stars', value='{0:.2f}★'.format(stars))
+                osuembed.add_field(name='Max Combo', value=maxcombo)
+                osuembed.add_field(name='CS', value='{0:.2f}'.format(cs))
+                osuembed.add_field(name='OD', value='{0:.2f}'.format(od))
+                osuembed.add_field(name='AR', value='{0:.2f}'.format(ar))
+                osuembed.add_field(name='HP', value='{0:.2f}'.format(hp))
 
-            embed.set_thumbnail(url='https://b.ppy.sh/thumb/'+ data[0]['beatmapset_id'] + '.jpg')
-            embed.set_author(name=data[0]['title'], url='https://osu.ppy.sh/s/' + data[0]['beatmapset_id'], icon_url='https://b.ppy.sh/thumb/'+ data[0]['beatmapset_id'] + '.jpg')
-            await self.bot.send_message(message.channel, embed=embed)
+                # Check for pp
+                if pp is not None:
+                    if not unsure:
+                        osuembed.add_field(name='PP', value='{0} with {1} ({2}).'.format(pp, mods, percent))
+                    else:
+                        osuembed.add_field(name='PP', value='{0} with {1} ({2}). Max Combo is null in osu api (based on 1500 combo)'.format(pp, mods, percent))
+                
+                osuembed.set_footer(text='Mode: {0}'.format(self.modes[data[0]['mode']]))
+
+            # Set thumbnail to map background and title to map name 
+            osuembed.set_thumbnail(url='https://b.ppy.sh/thumb/'+ data[0]['beatmapset_id'] + '.jpg')
+            osuembed.set_author(name=data[0]['title'], url='https://osu.ppy.sh/s/' + data[0]['beatmapset_id'], icon_url='https://b.ppy.sh/thumb/'+ data[0]['beatmapset_id'] + '.jpg')
+            await self.bot.send_message(message.channel, embed=osuembed)
+        else:
+            await self.bot.send_message(message.channel, 'osu! API seems to be down right now. Try again later')
 
     async def on_osu_message(self, message):
         ''' Check if beatmap was sent '''
