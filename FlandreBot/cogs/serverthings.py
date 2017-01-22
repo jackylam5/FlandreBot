e0d7bec62e9b601b9db3f08ed5a560e0c1ffe498
jacky <superjackylam5@hotmail.com>
2017-01-22 01:30:05 +0100

    added cleverbot

--- FlandreBot/cogs/serverthings.py
+++ FlandreBot/cogs/serverthings.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from FlandreBot.utils.IO import files
 from FlandreBot.utils import permissions
 from discord import Game
+from cleverbot import Cleverbot
 import os
 import time
 import json
@@ -13,6 +14,7 @@ class serverthings:
     def __init__(self, bot):
         self.bot = bot
         self.welcome = files("FlandreBot/data/serverthings/welcome.json", "load")
+        self.clever = files("FlandreBot/data/serverthings/clever.json", "load")
         self.settings = files("FlandreBot/config.json", "load")
     
     @commands.group(name="set", pass_context=True)
@@ -139,6 +141,46 @@ class serverthings:
         else:
             await self.bot.say("Please enable welcome message first")
     
+    @commands.group(name="clever", pass_context=True)
+    @permissions.checkAdmin()
+    async def _clever(self, ctx):
+        """Bot settings"""
+        if ctx.invoked_subcommand is None:
+            pages = self.send_cmd_help(ctx)
+            for page in pages:
+                await self.bot.send_message(ctx.message.channel, page)
+    
+    @_clever.command(pass_context=True, no_pm=True)
+    async def allow(self, ctx, *text : str): 
+        """allow server for talking to bot"""
+        server = ctx.message.server
+        channel = ctx.message.channel
+        if server.id not in self.clever:
+            self.clever[server.id] = {'channels:' : {}}
+            files("FlandreBot/data/serverthings/clever.json", "save", self.clever)
+            await self.bot.say("server added to list")
+        else:
+            self.clever.pop(server.id)
+            files("FlandreBot/data/serverthings/clever.json", "save", self.clever)
+            await self.bot.say("server removed from list")
+            
+    @_clever.command(pass_context=True, no_pm=True)
+    async def add(self, ctx, *text : str):         
+        """add channel to list"""
+        server = ctx.message.server
+        channel = ctx.message.channel
+        if server.id in self.clever:
+            if channel.id not in self.clever[server.id]:
+                self.clever[server.id][channel.id] = {'name' : channel.name}
+                files("FlandreBot/data/serverthings/clever.json", "save", self.clever)
+                await self.bot.say("channel added to list")
+            else:
+                self.clever[server.id].pop(channel.id)
+                files("FlandreBot/data/serverthings/clever.json", "save", self.clever)
+                await self.bot.say("channel removed from list")
+        else:
+            await self.bot.say("server is no on the list yet")
+            
     
     def send_cmd_help(self, ctx):
         if ctx.invoked_subcommand:
@@ -176,6 +218,15 @@ class serverthings:
                     if "?!server?!" in message:
                         message = message.replace("?!server?!", server.name)
                     await self.bot.send_message(channel, message)
+                    
+    async def on_mention_message(self, message):
+        if message.server.id in self.clever:
+            if message.channel.id in self.clever[message.server.id]:
+                if self.bot.user.mentioned_in(message):
+                    mess = message.content.split(' ', 1)[1]
+                    cleverbot = Cleverbot('FlandreBot')
+                    answer = cleverbot.ask(mess)
+                    await self.bot.send_message(message.channel, answer)
     
 def check_folders():
     if not os.path.exists("FlandreBot/data/serverthings"):
@@ -186,6 +237,9 @@ def check_files():
     if not os.path.isfile("FlandreBot/data/serverthings/welcome.json"):
         print("Creating empty welcome.json...")
         files("FlandreBot/data/serverthings/welcome.json", "save", {})
+    if not os.path.isfile("FlandreBot/data/serverthings/clever.json"):
+        print("Creating empty clever.json...")
+        files("FlandreBot/data/serverthings/clever.json", "save", {})
 
 def setup(bot):
     check_folders()
@@ -193,4 +247,5 @@ def setup(bot):
     n = serverthings(bot)
     bot.add_listener(n.member_join, "on_member_join")
     bot.add_listener(n.member_left, "on_member_remove")
+    bot.add_listener(n.on_mention_message, "on_message")
     bot.add_cog(n)
