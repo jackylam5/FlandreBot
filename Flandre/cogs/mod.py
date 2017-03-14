16eea275b3380d09e0defe1eeec5e1c81066c80c
Ma-wa-re <Ma-wa-re@users.noreply.github.com>
2017-03-14 12:20:51 +0000

    Added softban, rename and started cleanup command

--- Flandre/cogs/mod.py
+++ Flandre/cogs/mod.py
@@ -29,7 +29,7 @@ class mod:
                 json.dump({}, file)
             with open('Flandre/data/mod/past_names.json', 'w') as file:
                 json.dump({}, file)
-            with open('Flandre/data/mod/filter', 'w') as file:
+            with open('Flandre/data/mod/filter.json', 'w') as file:
                 json.dump({}, file)
             with open('Flandre/data/mod/ignore_list.json', 'w') as file:
                 json.dump({}, file)
@@ -154,6 +154,73 @@ class mod:
             if ctx.message.server.id in self.logging_channels:
                 log_channel = self.bot.get_channel(self.logging_channels[ctx.message.server.id])
                 await self.bot.send_message(log_channel, '{0.mention} has banned {1.mention}!\nReason: `{2}`'.format(author, user, reason))
+
+    @commands.command(no_pm=True, pass_context=True)
+    @permissions.checkMod()
+    async def softban(self, ctx, user : discord.Member, reason : str = ""):
+        ''' Softbans the user mentioned with reason. Uses as a kick that cleans up mesages
+            If server has a log channel it is logged
+        '''
+        author = ctx.message.author
+        server = ctx.message.server
+        try:
+            await self.bot.ban(user, 1)
+            await self.bot.unban(server, user)
+            await self.bot.say("Done. User softbanned for reason: `{0}`".format(reason))
+        except discord.errors.Forbidden:
+            await self.bot.say("Can't do that user has higher role than me")
+        except discord.errors.HTTPException:
+            await self.bot.say("Something went wrong. Please try again")
+        else:
+            if ctx.message.server.id in self.logging_channels:
+                log_channel = self.bot.get_channel(self.logging_channels[ctx.message.server.id])
+                await self.bot.send_message(log_channel, '{0.mention} has softbanned {1.mention}!\nReason: `{2}`'.format(author, user, reason))
+
+    @commands.command(no_pm=True, pass_context=True)
+    @permissions.checkMod()
+    async def rename(self, ctx, user : discord.Member, *, nickname : str = ''):
+        ''' Renames the mentioned user. No nickname given removes it
+        '''
+
+        if nickname == '':
+            nickname = None
+        else:
+            nickname = nickname.strip()
+
+        try:
+            await self.bot.change_nickname(user, nickname)
+            await self.bot.say("Users Nickname has been changed")
+        except discord.errors.Forbidden:
+            await self.bot.say("I can't do that. I lack the permissions to do so")
+        except dscord.errors.HTTPException:
+            await self.bot.say("Something went wrong. Please try again")
+
+    @commands.group(pass_context=True, no_pm=True)
+    @permissions.checkMod()
+    async def cleanup(self, ctx):
+        ''' Deletes messages
+
+            cleanup messages [number] - removes number amount of messages
+            cleanup user [name/mention] [number] - remove number amount of users messages
+            cleanup text "Text" [number] - removes number amount of messages with text in it
+        '''
+
+        if ctx.invoked_subcommand is None:
+            pages = self.send_cmd_help(ctx)
+            for page in pages:
+                await self.send_message(ctx.message.channel, page)
+
+    @cleanup.command(no_pm=True, pass_context=True)
+    async def text(self, ctx, text : str, number : int):
+        ''' Deletes the last X messages containing the text specified
+            Double quotes are needed for the text string
+
+            Example: cleanup text "Test" 5
+        '''
+
+        while deleted != number:
+
+
 
 def setup(bot):
     n = mod(bot)
