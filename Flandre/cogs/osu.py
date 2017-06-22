2db1b23c10ceceb00637c3c82b30e4965dbff10c
Ma-wa-re <Ma-wa-re@users.noreply.github.com>
2017-06-22 23:56:44 +0100

    Added top command for each gamemode
    Gets the users top 5 beatmaps for the gamemode asked for. Format might need looked at

--- Flandre/cogs/osu.py
+++ Flandre/cogs/osu.py
@@ -67,6 +67,33 @@ class Osu:
 
         return data[0] if data else {}
 
+    async def get_osu_user_top(self, user, mode=0):
+        ''' Gets the users top 5 scores'''
+
+        if not user:
+            return {}
+
+        key = self.config["api_key"]
+        url = f'{OSU_BASE_URL}/api/get_user_best?k={key}&u={user}&m={mode}&limit=5'
+        async with self.session.get(url) as resp:
+            data = await resp.json()
+
+        return data if data else {}
+
+    async def get_osu_beatmap(self, beatmap, mode=0):
+        ''' Gets beatmap info from the id given '''
+
+        if not beatmap:
+            return {}
+
+        key = self.config["api_key"]
+        url = f'{OSU_BASE_URL}/api/get_beatmaps?k={key}&b={beatmap}&m={mode}'
+        async with self.session.get(url) as resp:
+            data = await resp.json()
+
+        return data if data else {}
+
+
     def create_user_embed(self, user, mode=0):
         ''' Creates an embed about that user for that mode '''
 
@@ -90,7 +117,7 @@ class Osu:
                 f'Total Score: {total_score:,}')
 
         title = f'{user["username"]}'
-        url = f'https://osu.ppy.sh/u/{user["user_id"]}'
+        url = f'{OSU_BASE_URL}u/{user["user_id"]}'
         embed = discord.Embed(description=desc,
                               title=title,
                               url=url,
@@ -115,10 +142,38 @@ class Osu:
 
         return embed
 
+    async def create_top_embed(self, scores, name, mode=0):
+        ''' Creates the embed for the top 5 scores '''
+
+        url = f'{OSU_BASE_URL}u/{scores[0]["user_id"]}'
+
+        embed = discord.Embed(title=name, url=url, colour=16738740)
+        embed.set_author(name=f'Top Scores ({OSU_MODES[mode]})')
+        embed.set_thumbnail(url=f'https://a.ppy.sh/{scores[0]["user_id"]}')
+
+        for score in scores:
+            beatmap = await self.get_osu_beatmap(score['beatmap_id'], mode=mode)
+            beatmap = beatmap[0]
+
+            title = f'{beatmap["title"]} [{beatmap["version"]}]'
+            pp_raw = float(score['pp'])
+            total_score = int(score['score'])
+            page_link = f'{OSU_BASE_URL}/b/{score["beatmap_id"]}&m={mode}'
+            direct_link = f'osu://dl/{beatmap["beatmapset_id"]}'
+            info = (f'{RANK_EMOTES[score["rank"]]} '
+                    f'**{total_score:,} {pp_raw:.2f}pp** '
+                    f'`{score["count300"]}/{score["count100"]}/'
+                    f'{score["count50"]}/{score["countmiss"]}` '
+                    f'**{score["maxcombo"]}/{beatmap["max_combo"]}** '
+                    f'[Link]({page_link}) [osu!direct]({direct_link})')
+
+            embed.add_field(name=title, value=info, inline=False)
+
+        return embed
+
     # Standard mode
     @commands.group(name='osu')
     @commands.cooldown(60, 60.0, type=commands.BucketType.default)
-
     async def _osu(self, ctx):
         '''
         osu! commands
@@ -138,6 +193,18 @@ class Osu:
 
         if user:
             embed = self.create_user_embed(user)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('I couldn\'t find that user!')
+
+    @_osu.command(name='top')
+    @commands.cooldown(1, 10.0, type=commands.BucketType.default)
+    async def osu_top(self, ctx, *, name: str):
+        ''' Show basic user info for standard '''
+        top_scores = await self.get_osu_user_top(name)
+
+        if top_scores:
+            embed = await self.create_top_embed(top_scores, name)
             await ctx.send(embed=embed)
         else:
             await ctx.send('I couldn\'t find that user!')
@@ -163,6 +230,18 @@ class Osu:
         else:
             await ctx.send('I couldn\'t find that user!')
 
+    @_mania.command(name='top')
+    @commands.cooldown(1, 10.0, type=commands.BucketType.default)
+    async def mania_top(self, ctx, *, name: str):
+        ''' Show basic user info for mania '''
+        top_scores = await self.get_osu_user_top(name, 3)
+
+        if top_scores:
+            embed = await self.create_top_embed(top_scores, name, 3)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('I couldn\'t find that user!')
+
     # Taiko
     @_osu.group(name='taiko')
     async def _taiko(self, ctx):
@@ -184,6 +263,18 @@ class Osu:
         else:
             await ctx.send('I couldn\'t find that user!')
 
+    @_taiko.command(name='top')
+    @commands.cooldown(1, 10.0, type=commands.BucketType.default)
+    async def taiko_top(self, ctx, *, name: str):
+        ''' Show basic user info for taiko '''
+        top_scores = await self.get_osu_user_top(name, 1)
+
+        if top_scores:
+            embed = await self.create_top_embed(top_scores, name, 1)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('I couldn\'t find that user!')
+
     # CtB
     @_osu.group(name='ctb')
     async def _ctb(self, ctx):
@@ -201,6 +292,18 @@ class Osu:
 
         if user:
             embed = self.create_user_embed(user, 2)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('I couldn\'t find that user!')
+
+    @_ctb.command(name='top')
+    @commands.cooldown(1, 10.0, type=commands.BucketType.default)
+    async def ctb_top(self, ctx, *, name: str):
+        ''' Show basic user info for ctb '''
+        top_scores = await self.get_osu_user_top(name, 2)
+
+        if top_scores:
+            embed = await self.create_top_embed(top_scores, name, 2)
             await ctx.send(embed=embed)
         else:
             await ctx.send('I couldn\'t find that user!')
