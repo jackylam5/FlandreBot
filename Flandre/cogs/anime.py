69ebe5f8248b3dc5e154a57e4342f2f59ff976d6
Ma-wa-re <Ma-wa-re@users.noreply.github.com>
2017-06-24 22:17:28 +0100

    Add AnimePool and Show class
    Added the class that will hold all the anime and the class that holds the info about the show

--- Flandre/cogs/anime.py
+++ Flandre/cogs/anime.py
@@ -11,6 +11,36 @@ from dateutil.parser import parse
 
 from .. import permissions, utils
 
+class Show:
+    '''
+    This holds all the infomation we need about a anime
+    from anilist for the anime cog
+    '''
+
+    def __init__(self, anime):
+        self.id = anime.get('id')
+        self.title = anime.get('title_romaji')
+        self.english_title = anime.get('title_english')
+        self.type = anime.get('type')
+        self.total_episodes = anime.get('total_episodes')
+        self.image_url = anime.get('image_url_lge')
+        self.next_episode = anime['airing'].get('next_episode')
+        self.countdown = anime['airing'].get('countdown')
+        self.release_time = parse(anime['airing'].get('time'))
+        self._mal_link = None
+        self._crunchyroll_link = None
+
+    def recalculate_countdown(self, time_now):
+        ''' Recalculates the countdown from the current time '''
+
+        countdown = parse(self.release_time) - time_now
+        self.countdown = round(countdown.total_seconds())
+
+class AnimePool:
+
+    def __init__(self):
+        pass
+
 class Anime:
     '''
     Cog that has all anime and manga related items.
@@ -20,6 +50,10 @@ class Anime:
 
     def __init__(self, bot):
         self.bot = bot
+
+    async def __local_check(self, ctx):
+        ''' The cog disabled check '''
+        return utils.check_enabled(ctx)
 
 def setup(bot):
     ''' Add cog to bot '''
